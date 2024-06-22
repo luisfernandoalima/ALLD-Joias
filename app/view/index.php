d0cf09bb9b6859edac8d1c5010a2a7cf218acc02
Luis Fernando A. Lima <90354100+LuisFernandoALima@users.noreply.github.com>
2024-06-22 12:07:27 -0300

    Início da criação do header

---
+++ app/view/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/b95d68622e.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../assets/css/header/header.css">
+</head>
+
+<body>
+    <aside>
+
+    </aside>
+    <header>
+        <div class="logo-img">
+            <a href="">
+                <img src="../assets/img/objects/ALLDLogoSmall.png" alt="Logo ALLD">
+            </a>
+        </div>
+        <div class="search-bar-div">
+            <form action="" method="get" class="search-bar-forms">
+                <input type="text" name="pesquisa" id="search-bar" placeholder="Busque por joias, alianças...">
+                <button class="search-bar-button">Buscar <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
+            </form>
+        </div>
+        <div class="service-div">
+            <div class="user-div">
+                <a href="">
+                    <div class="user-icon">
+                        <i class="fa-solid fa-user" style="color: #000000;"></i>
+                    </div>
+                    <div class="user-text">Minha Conta</div>
+                </a>
+            </div>
+            <div class="cart-div">
+                <a href="">
+                    <div class="cart-icon">
+                        <i class="fa-solid fa-cart-shopping" style="color: #000000;"></i>
+                        <span class="cart-number"></span>
+                    </div>
+                    <div class="cart-text">
+                        Carrinho
+                    </div>
+                </a>
+            </div>
+        </div>
+    </header>
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- JS -->
+    <script type="module" src="../assets/js/menu.js"></script>
+</body>
+
+</html>
